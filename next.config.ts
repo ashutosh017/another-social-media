1f46b24dad2d142f7e23725d6ca316d7bf78cbaf
ashutosh017 <wwwashutosh225@gmail.com>
2025-07-19 16:14:26 +0530

    chore: disable eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint:{
+    ignoreDuringBuilds:true
+  },
   images: {
     remotePatterns: [
       new URL("https://media.newyorker.com/**"),
