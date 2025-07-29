f14a227d91f05767754d44a1f709689526d3d0a8
ashutosh017 <wwwashutosh225@gmail.com>
2025-07-29 09:56:48 +0530

    add: prisma accelerate

--- lib/db.ts
+++ lib/db.ts
@@ -1,21 +1,19 @@
 // import { PrismaClient } from "@/app/generated/prisma";
-// import { withAccelerate } from "@prisma/extension-accelerate";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 import { PrismaClient } from "@/app/generated/prisma";
 
 // const globalForPrisma = global as unknown as { prisma:  PrismaClient };
 
-// const prisma =
-//   globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate());
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const prisma =
+  globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate());
 
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 // export default prisma;
 
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
+// const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
