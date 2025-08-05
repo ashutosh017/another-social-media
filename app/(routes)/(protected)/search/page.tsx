5b45eb9f5e9e86cccc8222666484931a9fd2b5ff
ashutosh017 <wwwashutosh225@gmail.com>
2025-08-06 00:05:30 +0530

    fix: remove delay

--- app/(routes)/(protected)/search/page.tsx
+++ app/(routes)/(protected)/search/page.tsx
@@ -3,10 +3,5 @@ import SearchPage from "./search-page";
 
 export default async function page() {
   const searchFeed = await fetchSearchFeed();
-   await new Promise((res) => {
-    setTimeout(() => {
-      res(1);
-    }, 2000);
-  });
   return <SearchPage initialSearchFeed={searchFeed} />;
 }
