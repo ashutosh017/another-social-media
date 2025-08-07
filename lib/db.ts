b73b2dc118f872b58ab11300d29e66ba2c0cfcbc
ashutosh017 <wwwashutosh225@gmail.com>
2025-08-07 23:59:39 +0530

    fix: db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,6 @@
 // import { PrismaClient } from "@/app/generated/prisma";
 // import { withAccelerate } from "@prisma/extension-accelerate";
 
-import { PrismaClient } from "./generated/prisma";
-
 // const globalForPrisma = global as unknown as { prisma:  PrismaClient };
 
 // const prisma =
@@ -12,10 +10,25 @@ import { PrismaClient } from "./generated/prisma";
 
 // export default prisma;
 
+// import { PrismaClient } from "./generated/prisma";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+// const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
+
+import { PrismaClient } from "./generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
